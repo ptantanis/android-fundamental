d55588fe4ed33d5b0c25bead0eeffe861e4ea3bf
ptantanis <pachara.tantanis@thoughtworks.com>
2020-07-02 18:52:55 +0700

    finished about me interactive

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        done_button.setOnClickListener { addNickName(it) }
+        nickname_text.setOnClickListener { updateNickname(it) }
+    }
+
+    private fun addNickName(view: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        nickname_text.text = nickname_edit.text
+        nickname_edit.visibility = View.GONE
+        view.visibility = View.GONE
+        nickname_text.visibility = View.VISIBLE
+    }
+
+    private fun updateNickname (view: View) {
+        nickname_edit.visibility = View.VISIBLE
+        done_button.visibility = View.VISIBLE
+        view.visibility = View.GONE
+        nickname_edit.requestFocus()
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(nickname_edit, 0)
     }
 }
